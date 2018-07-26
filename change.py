467fac6e6ec0490d7fe8ad23d6dc43fd67befe44
Russzheng <marshzhengzhe@icloud.com>
2018-07-26 20:57:01 +0800

    python3 modification, gmail to icloud since gmail is banned in mainland china

--- change.py
+++ change.py
@@ -20,15 +20,15 @@ def send_email(user, pwd, recipient): #snippet courtesy of david / email sending
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587) #start smtp server on port 587
+        server = smtplib.SMTP("smtp.mail.me.com", 587) #start smtp server on port 587
         server.ehlo()
         server.starttls()
         server.login(gmail_user, gmail_pwd) #login to gmail server
         server.sendmail(FROM, TO, message) #actually perform sending of mail
-        server.close() #end server
-        print '[+]Successfully sent email notification' #alert user mail was sent
-    except Exception, e: #else tell user it failed and why (exception e)
-        print "[-]Failed to send notification email, " +str(e)
+        #server.close() #end server
+        print('[+]Successfully sent email notification') #alert user mail was sent
+    except: #else tell user it failed and why (exception e)
+        print("[-]Failed to send notification email")
 
 def sendtweet(consumer_key, consumer_secret,access_token, access_token_secret,status_string):
     try:
@@ -37,41 +37,39 @@ def sendtweet(consumer_key, consumer_secret,access_token, access_token_secret,st
         api = tweepy.API(auth)
         api.update_status(status_string)
     except tweepy.error.TweepError:
-        print "[-]Error, invalid or expired twitter tokens, visit http://apps.twitter.com to retrieve or refresh them"
+        print("[-]Error, invalid or expired twitter tokens, visit http://apps.twitter.com to retrieve or refresh them")
 
 def sendtext(message):
-    print "[+]Sending text message...."
+    print("[+]Sending text message....")
     try:
         twilioCli = Client(accountSID, authToken)
         twilioCli.messages.create(body=message, from_=twilioNumber, to=myNumber)
-    except Exception, e:
-        print "[-]Error " +e
+    except:
+        print("[-]Error " )
 
 def main():
-        print "[+]Starting up monitor on " +url
-        print "[+]Email on change detect is set to " +str(notify)
-        print "[+]Tweeting is set to "+str(tweet)
-        print "[+]Text notifications set to "+str(text)
+        print("[+]Starting up monitor on " +url)
+        print("[+]Email on change detect is set to " +str(notify))
 
         with requests.Session() as c:
             try:
                 page1 = c.get(url,headers={'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"}) #base page that will be compared against
-            except Exception, e:
-                print "[-]Error Encountered during initial page retrieval: " +e
+            except:
+                print("[-]Error Encountered during initial page retrieval: ")
 
             while 1:
                     time.sleep(wait_time) #wait beetween comparisons
                     try:
                         page2 = c.get(url, headers={'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"}) #page to be compared against page1 / the base page
-                    except Exception, e:
-                        print "[+]Error Encountered during comparison page retrieval: " +e
+                    except:
+                        print("[+]Error Encountered during comparison page retrieval: " )
 
                     if page1.content == page2.content: #if else statement to check if content of page remained same
-                        print '[-]No Change Detected on ' +str(url)+ "\n" +str(datetime.now())
+                        print('[-]No Change Detected on ' +str(url)+ "\n" +str(datetime.now()))
                     else:
                         status_string = 'Change Detected at ' +str(url)+ "\n" +str(datetime.now())
                         message = status_string
-                        print "[+]"+status_string
+                        print("[+]"+status_string)
                         if notify == True:
                             send_email(user, pwd, recipient) #send notification email
                         else:
@@ -82,7 +80,11 @@ def main():
                             pass
                         if text == True:
                             sendtext(message)
-                        print '\n[+]Retrieving new base page and restarting\n'
+                        print('\n[+]Retrieving new base page and restarting\n')
                         main()
 if __name__ == '__main__':
-    main()
+    #main()
+    start_time = time.time()
+    send_email(user, pwd, recipient)
+    print("--- %s seconds passed ---" % (time.time() - start_time))
+
